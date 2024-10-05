30a5bac8b9bb780343e06eea9daca265e56cb1f4
LOGiC31 <singh.v357@gmail.com>
2024-10-04 21:19:49 -0500

    zeit issue fix

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
-# class Event < ApplicationRecord
-#   belongs_to :item
-#   belongs_to :associated_user, class_name: "User", foreign_key: "associated_user_id"
-#   belongs_to :associated_student, class_name: "User", foreign_key: "associated_student_id", optional: true
-# end
+class Event < ApplicationRecord
+  belongs_to :item
+  belongs_to :associated_user, class_name: "User", foreign_key: "associated_user_id"
+  belongs_to :associated_student, class_name: "User", foreign_key: "associated_student_id", optional: true
+end
