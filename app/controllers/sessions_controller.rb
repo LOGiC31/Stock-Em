ae756e5816ad567a323cba6b666a81c9598bd959
Simran Jot Kaur <simranjotkaur@Simrans-MacBook-Pro-2.local>
2024-10-04 23:45:24 -0500

    rspec covered

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,6 +11,8 @@ class SessionsController < ApplicationController
   skip_before_action :require_login, only: [:omniauth]
   def omniauth
     auth = request.env['omniauth.auth']
+    puts('asdfasdfasdfadf ================+>>>>>>')
+    puts(auth)
     @user = User.find_or_create_by(user_id: auth['uid'], provider: auth['provider']) do |u|
       u.email = auth['info']['email']
       names = auth['info']['name'].split
