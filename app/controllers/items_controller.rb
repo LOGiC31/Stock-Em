f0a7752b5eeb8504df848e7949af9725c83dd236
ishaan <ishaanrawal@gmail.com>
2024-10-15 02:35:34 -0500

    added search functionality

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,7 +5,23 @@ class ItemsController < ApplicationController
   # get all the items
   def index
     @items = Item.all
-    puts(@items.inspect)
+
+    # Debugging: Check if query parameter is present
+    puts "Search query: #{params[:query]}"
+
+    if params[:query].present?
+      query = params[:query].downcase
+      @items = @items.select do |item|
+        item.item_name.downcase.include?(query) || item.category.downcase.include?(query)
+      end
+    end
+
+    # Debugging: Check filtered results
+    puts "Filtered items: #{@items.map(&:item_name)}"
+
+    if params[:available_only] == '1'
+      @items = @items.select(&:currently_available)
+    end
   end
 
   # get specific item
