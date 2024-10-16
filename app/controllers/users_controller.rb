4a24a772121c3d630c22b78c6f203528cf3e6fb1
Simran Kaur <simrankaur@tamu.edu>
2024-10-16 14:14:53 -0500

    Profile edit changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,25 @@ class UsersController < ApplicationController
   def show
     @current_user = User.find(params[:id])
   end
+
+  def update
+    @current_user = User.find(params[:id])
+
+    respond_to do |format|
+      if @current_user.update(user_params)
+        format.html { redirect_to user_profiles_path, notice: "User was successfully updated." }
+        format.json { render :show, status: :ok, location: @current_user }
+      else
+        format.html { render :show, status: :unprocessable_entity }
+        format.json { render json: @current_user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :uin, :email, :contact_no, :role, :details)
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,11 @@ Rails.application.routes.draw do
 
   resources :items
   resources :user_profiles
+  resources :users
+
 
   root 'welcome#index'
   get 'welcome/index', to: 'welcome#index', as: 'welcome'
-  get '/users/:id', to: 'users#show', as: 'user'
 
   get '/logout', to: 'sessions#logout', as: 'logout'
   get '/auth/google_oauth2/callback', to: 'sessions#omniauth'
