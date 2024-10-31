4fdf0733a9b6038ed08f8124e60cfedf746112a4
simranjotkaur6 <simranjotkaur@tamu.edu>
2024-10-30 20:18:59 -0500

    test cases fixes and improve coverage (#89)
    * update master with latest dev (#75)
    
    * Adding Sprint 2 Documentation
    
    * added search functionality
    
    * Welcome Page enhanced and some css fixes
    
    * Name bug fixed
    
    * User Profile Page name display fixed
    
    * Some CSS changes
    
    * cucumber test for search and filter items
    
    * added rspec tests for search and filter
    
    * Profile edit changes
    
    * user profile css changes
    
    * adding route for show item
    
    * adding link to items path
    
    * Convert Layout to Use HTML5 Semantic Elements
    
    * adding header and footer
    
    * updating footer
    
    * adding css template
    
    * adding buttons for edit and back
    
    * adding styling for main text
    
    * adding styling for header
    
    * adding styling for footer
    
    * adding styling for button hover
    
    * adding styling for image
    
    * adding flexibility for left column in image details
    
    * updating notes
    
    * changing items view to list
    
    * rsoec done for user profile
    
    * adding flash messages rendering
    
    * adding css for flash messages
    
    * adding event listener for flash message
    
    * updating config
    
    * Primitive Features
    
    * Clean up prev. commit
    
    * Display notes, unformatted
    
    * Mark's features
    
    * adding more seed data
    
    * Add search and filter function for items page (#59)
    
    * Added Sprint 2 Plan and Sprint 1 Retrospective (#48)
    
    * added search functionality
    
    * cucumber test for search and filter items
    
    * added rspec tests for search and filter
    
    ---------
    
    Co-authored-by: SourPatch555 <tanishq.chopra@tamu.edu>
    
    * create new item feature added with rspec and cucumber
    
    * adding scrolling for notes
    
    * cucumber tests done
    
    * remove rspec step for invalid creds
    
    * focused field margin fixed
    
    * fixed search UI
    
    * fixing some bugs and add update item status feature (#73)
    
    * initial commit - add status and comment in items
    
    * update status
    
    * css fix
    
    * fixing rubocop issues
    
    * heroku changes
    
    * correcting flash message
    
    * debug msg
    
    * remove debug msgs
    
    ---------
    
    Co-authored-by: SourPatch555 <tanishq.chopra@tamu.edu>
    Co-authored-by: ishaan <ishaanrawal@gmail.com>
    Co-authored-by: Simran Kaur <simrankaur@tamu.edu>
    Co-authored-by: Alisha Raj <alisharaj@Alishas-MacBook-Air.local>
    Co-authored-by: alisharaj2607Tamu <alisharaj2607@tamu.edu>
    Co-authored-by: sturtevm-tamu <sturtevm@tamu.edu>
    Co-authored-by: Mark Sturtevant <157788748+sturtevm-tamu@users.noreply.github.com>
    Co-authored-by: Ishaan Singh Rawal <39989567+israwal@users.noreply.github.com>
    Co-authored-by: Simran Jot Kaur <simranjotkaur@Simrans-MacBook-Pro-2.local>
    Co-authored-by: simranjotkaur6 <simranjotkaur@tamu.edu>
    
    * Update README.md
    
    * Add files via upload
    
    * Update README.md
    
    * setting up authorization dashboard
    
    * test case fixes and coverage fix
    
    ---------
    
    Co-authored-by: Vinay Singh <43707871+LOGiC31@users.noreply.github.com>
    Co-authored-by: SourPatch555 <tanishq.chopra@tamu.edu>
    Co-authored-by: ishaan <ishaanrawal@gmail.com>
    Co-authored-by: Simran Kaur <simrankaur@tamu.edu>
    Co-authored-by: Alisha Raj <alisharaj@Alishas-MacBook-Air.local>
    Co-authored-by: alisharaj2607Tamu <alisharaj2607@tamu.edu>
    Co-authored-by: sturtevm-tamu <sturtevm@tamu.edu>
    Co-authored-by: Mark Sturtevant <157788748+sturtevm-tamu@users.noreply.github.com>
    Co-authored-by: Ishaan Singh Rawal <39989567+israwal@users.noreply.github.com>
    Co-authored-by: Simran Jot Kaur <simranjotkaur@Simrans-MacBook-Pro-2.local>
    Co-authored-by: LOGiC31 <singh.v357@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@
 # Similar to user profiles, just defines a way to showcase a profile.
 class UsersController < ApplicationController
   skip_before_action :require_login, only: [:update_auth_level]
+  skip_before_action :require_login, only: [:update_auth_level]
   def show
     @current_user = User.find(params[:id])
   end
@@ -24,7 +25,7 @@ class UsersController < ApplicationController
   def update_auth_level
     user = User.find(params[:id])
 
-    if user.update(auth_level: params[:auth_level], role: user.role)
+    if params[:auth_level] < "3" && user.update(auth_level: params[:auth_level], role: user.role)
       render json: { auth_level: user.auth_level, role: user.role }
     else
       render json: { status: 'error', message: user.errors.full_messages }, status: :unprocessable_entity
