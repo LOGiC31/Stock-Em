9c65b559d73b2118039050087447709f2991a8c9
ishaan <ishaanrawal@gmail.com>
2024-11-13 15:22:09 -0600

    location dropdown and others

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,19 +5,35 @@ class EventsController < ApplicationController
   def publish_event # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
     details = ''
     time_string = Time.now.strftime('%Y-%m-%d %H:%M:%S')
+
+    # Custom location logic
+    location = params[:location].presence || 'Unknown Location' # Default to 'Unknown Location' if no location is provided
+    # Check if 'Other' is selected and apply custom location logic
+    if params[:location] == 'others'
+      if params[:custom_location].blank?
+        # Throw an error or redirect with a message if the custom location is blank
+        flash[:error] = "Custom location can't be blank when 'Other' is selected."
+        @item = Item.find(params[:itemid])
+        redirect_to @item
+        return
+      else
+        location = params[:custom_location] # Use the custom location provided by the user
+      end
+    end
+
     if params[:evtype] == 'Checkout'
       details = 'Checked out to '
 
       details += "#{params[:teams]} in " unless params[:teams].blank?
-
-      details += "#{params[:location]} "
+      details += " #{location} " #"#{params[:location]} "
       details += "on #{time_string} "
       details += "with #{params[:professor]} as the responsible professor"
 
     else
       details = "Checked in on #{time_string} "
       details += "to #{params[:professor]} "
-      details += "with the new location being #{params[:location]}"
+      details += "with the new location being #{location}"
+      # details += "with the new location being #{params[:location]}"
 
     end
     details += ".  #{params[:comments]}" unless params[:comments].blank?
