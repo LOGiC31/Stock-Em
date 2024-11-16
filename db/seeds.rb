26c1481806838b843a52898a4cd1d4144e9233c0
Simran Jot Kaur <simranjotkaur@Simrans-MacBook-Pro-2.local>
2024-11-15 18:38:32 -0600

    Removing seeded users

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,17 +8,6 @@
 # Clear existing data to avoid duplication
 
 # Create Users
-users = [
-  { user_id: 'USR001', name: 'Philip Ritchey', uin: '123456', email: 'philip@example.com', contact_no: '5551234',
-    role: 'professor_admin', auth_level: 2 },
-  { user_id: 'USR002', name: 'Paul Taele', uin: '654321', email: 'paul@example.com', contact_no: '5555678',
-    role: 'professor_admin', auth_level: 2 },
-  { user_id: 'USR003', name: 'random TA', uin: '987654', email: 'randomTA@example.com', contact_no: '5559876',
-    role: 'assistants', auth_level: 1 }
-]
-
-users.each { |user| User.create!(user) }
-
 Admin.find_or_create_by(username: 'admin') do |admin|
   admin.password = 'admin' # Raw password
 end
